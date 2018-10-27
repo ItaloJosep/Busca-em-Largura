5b24ea2b223177f2a31a2338a45302cffb8d45ba
Italo Josep <italojosep@gmail.com>
2018-10-27 17:22:02 -0300

    Set e get Lista

--- Busca_Lagura/Lista/Fila.cs
+++ Busca_Lagura/Lista/Fila.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Busca_Lagura.Grafo;
 
 namespace Busca_Lagura.Lista
 {
 	public class Fila
 	{
+		public Elemento Inicio { get; set; }
+		public Elemento Fim { get; set; }
+		public int QtdAtendidos { get; set; }
+
+		public Fila()
+		{
+			Inicio = null;
+			Fim = null;
+			QtdAtendidos = 0;
+		}
+
+		public void ColocarFila(Vertice vertice)
+		{
+			Elemento novo = new Elemento
+			{
+				Vertice = vertice,
+				Proximo = null
+			};
+
+			if (Fim != null)
+				Fim.Proximo = novo;
+			else
+				Inicio = novo;
+
+			Fim = novo;			
+		}
+
+		public Vertice BuscaPrimeiro()
+		{	
+			if (Inicio != null)
+			{
+				Elemento retorno = Inicio;
+				Inicio = Inicio.Proximo;
+
+				if (Inicio == null)
+					Fim = null;
+
+				return retorno.Vertice;
+			}
+
+			return null;
+		}
 
 	}
 }
